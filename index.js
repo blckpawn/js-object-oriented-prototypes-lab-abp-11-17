7e3745ad268bcc992436c5ad93b7d76066442c3a
Brian Winter <blackpawn8990@gmail.com>
2017-11-12 20:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
+}
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
@@ -12,11 +12,11 @@ function BoardMember(name, homeState, training){
 //   this.doCharity = function(){
 //     return "I like to help people."
 //   }
-// 
+//
 //   this.releasePressStatement = function(){
 //     return "You will see great things from Scuber."
 //   }
-// 
+//
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
